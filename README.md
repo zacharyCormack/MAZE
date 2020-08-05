4e04ab37bbc2b70f3591eebbf3a15fda8924434b
Zachary Runge Cormack <cormack.zachary@gmail.com>
2020-08-05 11:35:57 -0400

    wrote README.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # MAZE
+
+<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+
+## Introduction
+
+This project creates a maze and allows the user to travel within it.
+The player will have some number of steps to travel through the maze.
+This is determined by the quadratic function \\( \left\lfloor t=\frac{s^2}{8}+s+8 \right\rfloor \\).
+
+## How it Works
+
+The `main.cpp` file handles setup, getting game size, timer, and checking for game end.
+It runs the `make_maze` function from the `Maze.cpp` file.
+The `Maze.cpp` file generates a random maze. The maze is a mathematical graph, a tree.
+All nodes have 0 to 2 child nodes.
+Trees of this format can be created by the `make_maze` function.
+It uses the recursive `link_rooms` function to connect the start and end nodes together, with the call:
+```cpp
+link_rooms(0, n-1, &m);
+```
+Then `link_rooms` may call itself to connect room 0 to a, and a to n-1, and the process continues.
+
+## In Use
+
+![example GIF](example.gif "EXAMPLE")
--- main.cpp
+++ main.cpp
@@ -2,17 +2,32 @@
 
 int main()
 {
-	cout << "\nWELCOME TO THE MAZE.\nGame size: ";
-	unsigned size;
-	cin >> size;
-	Maze game = make_maze(size);
-	unsigned short timer=51;
-	Point point = game.points[0];
-	cout << "\n\e[s";
-	while(step(&point, &game)&&--timer)
-		cout << "\e[u\e[KTIMER: " << timer << "\n";
-	cout << "\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K";
-	cout << "\nYOU" << (timer?"WIN":"LOSE") << "!\n";
-	cin.ignore();
+	bool play_again=true;
+	while (play_again)
+	{
+		cout << "\nWELCOME TO THE MAZE.\nGame size: ";
+		unsigned size;
+		cin >> size;
+		Maze game = make_maze(size);
+		unsigned short timer=size*size/8+size+8;
+		Point point = game.points[0];
+		cout << "\n\e[s";
+		while(step(&point, &game)&&--timer)
+			cout << "\e[u\e[KTIMER: " << timer << "\n";
+		cout << "\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K\e[A\e[K";
+		cout << "\nYOU " << (timer?"WIN":"LOSE") << "!\n";
+		cin.ignore();
+		string ans = "";
+		while (ans != "y")
+		{
+			cout << "\nPlay again? y/n: ";
+			cin >> ans;
+			if (ans == "n")
+			{
+				play_again=false;
+				break;
+			}
+		}
+	}
 	return 0;
 }
